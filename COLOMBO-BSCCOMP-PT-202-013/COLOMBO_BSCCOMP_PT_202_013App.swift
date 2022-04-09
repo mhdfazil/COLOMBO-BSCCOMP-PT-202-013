f76c46715982dcdaf16e822feabd546ad03f77b5
Fazil <mhdfazil79@gmail.com>
2022-04-10 00:12:58 +0530

    initial commit

---
+++ COLOMBO-BSCCOMP-PT-202-013/COLOMBO_BSCCOMP_PT_202_013App.swift
@@ -0,0 +1,17 @@
+//
+//  COLOMBO_BSCCOMP_PT_202_013App.swift
+//  COLOMBO-BSCCOMP-PT-202-013
+//
+//  Created by Mohamed Fazil on 2022-04-09.
+//
+
+import SwiftUI
+
+@main
+struct COLOMBO_BSCCOMP_PT_202_013App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COLOMBO-BSCCOMP-PT-202-013/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COLOMBO-BSCCOMP-PT-202-013
+//
+//  Created by Mohamed Fazil on 2022-04-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
